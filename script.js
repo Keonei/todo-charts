1aea61905d91faee05496a2c5c5757fb83278d6a
kerry <keonei@uw.edu>
2017-06-22 02:28:22 -0700

    inserted google chart into html, added js, chart loading not populating

--- script.js
+++ script.js
@@ -2,76 +2,114 @@ console.log("Epic Adventure Time!");
 
 // chart
 
-// Load the Visualization API and the corechart package.
-google.charts.load('current', {'packages': ['corechart']});
-// Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawMultSeries);
+var classes = [
+  "Paladin",
+  "Cleric",
+  "Ranger",
+  "Rogue",
+  "Fighter",
+  "Wizard"
+];
 
-function drawMultSeries() {
-      var data = google.visualization.arrayToDataTable([
-        ['Paladin' taskList.members],
-        ['Cleric' taskList.members],
-        ['Ranger' taskList.members],
-        ['Rogue,' taskList.members],
-        ['Fighter' taskList.members],
-        ['Wizard' taskList.members]
-      ]);
+window.onload = function() {
 
+  function buildChart(tasks) {
+
+      var dataChart = [];
+
+        for (var x = 0; x < classes.length; x++){
+          dataChart.push(0);
+          }
+          // loop through and get data for the tasks array
+          for (var i = 0; i < tasks.length; i++) {
+            dataChart[tasks[i].members]++;
+          }
+          // build the array for our chart
+          var rows = new Array ();
+          for (var y = 0; y < dataChart.length; y++) {
+            rows.push( [classes[y], dataChart[y]] );
+          }
+          console.log(rows);
+    // Load the Visualization API and the corechart package.
+    google.charts.load (
+      'current',
+      {'packages': ['corechart']
+    });
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+
+      // Create the data table.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Class');
+      data.addColumn('number', 'Chosen');
+      data.addRows(rows);
+
+      // Set chart options
       var options = {
-        title: 'What classes are already in play',
+        title:'Chosen',
         hAxis: {
-          title: 'Classes',
+          title: 'Whats in play'
         },
         vAxis: {
-          title: 'How Many of you'
+          title: 'Class'
         }
       };
 
-      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-    }
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.ColumnChart (
+        document.getElementById('chart_div'));
 
+        chart.draw(data, options);
+    };
+  }
 
-// create an object array for our selections
-var taskList = {
-  members: [
+  // create an object array for our selections
+  var tasks = {
+    members: [
 
-]};
+  ]};
+  console.log(tasks);
+  // creates the unordered list for output
+  function formData () {
+    var teamList = document.getElementById("team-list")
+    // reset the page - not working ask why
+    // document.getElementsByTagName("form").reset();
 
-// creates the unordered list for output
-function formData () {
-  var teamList = document.getElementById("team-list")
-  // reset the page - not working ask why
-  // document.getElementsByTagName("form").reset();
+    var ul = document.createElement("ul")
+      for(var i = 0; i < tasks.members.length; i++) {
+        console.log(tasks.members[i])
 
-  var ul = document.createElement("ul")
-    for(var i = 0; i < taskList.members.length; i++) {
-      console.log(taskList.members[i])
+        var li = document.createElement("li");
+          li.innerHTML = tasks.members[i]
+          // append the li to the ul elements
+          ul.appendChild(li)
+        }
+        // append the ul to the parent div
+        teamList.appendChild(ul)
 
-      var li = document.createElement("li");
-        li.innerHTML = taskList.members[i]
-        // append the li to the ul elements
-        ul.appendChild(li)
+        buildChart(tasks);
       }
-      // append the ul to the parent div
-      teamList.appendChild(ul)
-    }
 
-function addList () {
-  event.preventDefault();
-  // locate form section and find our values
-  // lets use querySelector since there is only one form
-  var form = document.querySelector("form");
-  // our task in this case is player selection for our team, input our values
-  var taskMember = form.teamMember.value + "\xa0has chosen to be the\xa0" + form.memberClass.value + "\xa0for your party and has requested difficulty\xa0" + document.getElementById("difficulty").value;
-  // push selection to our array
-  taskList.members.push(taskMember);
-  formData();
-}
+  function addList () {
+    event.preventDefault();
+    // locate form section and find our values
+    // lets use querySelector since there is only one form
+    var form = document.querySelector("form");
+    // our task in this case is player selection for our team, input our values
+    var taskMember = form.teamMember.value + "\xa0has chosen to be the\xa0" + form.memberClass.value + "\xa0for your party and has requested difficulty\xa0" + document.getElementById("difficulty").value;
+    // push selection to our array
+    tasks.members.push(taskMember);
+    formData();
 
-window.onload = function() {
-  formData();
+    form.reset();
+  }
 
-  var form = document.querySelector("form")
-  form.onsubmit = addList;
+    var form = document.querySelector("form")
+    form.onsubmit = addList;
 }
